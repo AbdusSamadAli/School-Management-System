0e1c6cb366ae7ff87266c64aff3ab74bb3de9a0d
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-03-18 22:23:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 cd /home/ubuntu/nextjs-app
 npm install --production
 pm2 stop nextjs-app || true
-pm2 start npm --name "nextjs-app" -- start
+pm2 start npm --name "nextjs-app" -- start -- -p 3000 -H 0.0.0.0
 
