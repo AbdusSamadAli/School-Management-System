862f7c78d5d8b55be65dd7cc937b3f2d30b31d82
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-03-19 22:50:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-cd /home/ubuntu/School-Management-System || exit
-pm2 stop nextjs-app || true
-pm2 start npm --name "School-Management-System" -- start -- -p 3000 -H 0.0.0.0
 
+cd /home/ubuntu/School-Management-System || exit
+
+# Install dependencies
+npm install --production
+
+# Build Next.js app
+npm run build
+
+# Stop the previous PM2 process (if running)
+pm2 stop School-Management-System || true
+
+# Start the app with PM2
+pm2 start npm --name "School-Management-System" -- start -- -p 3000 -H 0.0.0.0
