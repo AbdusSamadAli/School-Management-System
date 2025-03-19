e397e851a3ef0ab1a511ab629e26f98e4595d1d3
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-03-19 21:07:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/School-Management-System || exit
+cd /home/ubuntu/nextjs-app || exit
 pm2 stop nextjs-app || true
 pm2 start npm --name "nextjs-app" -- start -- -p 3000 -H 0.0.0.0
 
